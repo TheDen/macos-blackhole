140eae7ecdceabe839c89c288fd05bc473b8c7de
TheDen <deniskhoshaba@gmail.com>
2020-02-13 02:03:51 +1100

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# macos-blackhole
+
+## What is is
+
+A simple bash script I use to:
+
+* Download and install the simple hosts file manager [Gas Mask](https://github.com/2ndalpha/gasmask) 
+* Download [StevenBlack's](https://github.com/StevenBlack/hosts) well-curated host files to load within Gas Mask
+* If Gas Mask is already installed, the host files will be updated to the latest versions
+
+## Why not just use [pi-Hole](https://github.com/pi-hole/pi-hole)?
+
+Sometimes pi-hole can't be deployed on networks you have control of (or unstrusted networks). This allows mobility with respect to host blocking.
+
---
+++ setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -euo pipefail
+
+## Gas Mask Variables
+gasmask_version="0.8.5"
+gasmask_zip="gas_mask_${gasmask_version}.zip"
+gasmask_url="https://github.com/2ndalpha/gasmask/releases/download/${gasmask_version}/${gasmask_zip}"
+gasmask_checksum="e62755aa8c8466c4149d37057f1a29cef8bae7fe77949fa9e1123eebf9b3b0b41b9e05a9d1f098dc775c5c65be12d2baf51321e70df069a967d35b4951cc2f0f  ${gasmask_zip}"
+gasmask_local_dir="${HOME}/Library/Gas Mask/Local"
+temp_folder=$(mktemp -d)
+
+## Links to hosts from https://github.com/StevenBlack/hosts
+# Unified hosts = (adware + malware)
+unified_hosts="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+# Unified hosts + fakenews + gambling + porn + social
+unified_hosts_fakenews_gambling_porn_social="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts"
+
+# Cleanup function
+cleanup() {
+  if [ -n "${temp_folder}" ]; then
+    rm -rf "${temp_folder}"
+  fi
+}
+trap 'cleanup' EXIT
+trap 'cleanup HUP' HUP
+trap 'cleanup TERM' TERM
+trap 'cleanup INT' INT
+
+download_and_install_gas_mask() {
+  echo "Dowloading Gas Mask..."
+  curl -sL "${gasmask_url}" -o "${temp_folder}/${gasmask_zip}"
+  echo "${temp_folder}/${gasmask_zip}"
+  (
+    cd "${temp_folder}"
+    echo "${gasmask_checksum}" | shasum -a 512 -c
+  )
+  echo "Installing Gas Mask..."
+  unzip -oq "${temp_folder}/${gasmask_zip}" -d "/Applications/"
+}
+
+start_gas_mask() {
+  "/Applications/Gas Mask.app/Contents/Resources/Launcher.app/Contents/MacOS/Launcher" || true
+}
+
+kill_gas_mask() {
+  pkill -x "Gas Mask" || true
+}
+
+download_hostfile() {
+  echo "Downloading hostfile ${2} to ${gasmask_local_dir}"
+  curl -sL "$1" -o "${gasmask_local_dir}/${2}.hst"
+}
+
+restart_gas_mask() {
+  echo "Restarting Gas Mask..."
+  start_gas_mask
+  kill_gas_mask
+}
+
+if [ ! -d "/Applications/Gas Mask.app/" ]; then
+  download_and_install_gas_mask
+fi
+
+restart_gas_mask
+
+until [ -d "${gasmask_local_dir}" ]; do
+  sleep 2
+done
+
+download_hostfile "${unified_hosts}" "${!unified_hosts@}"
+download_hostfile "${unified_hosts_fakenews_gambling_porn_social}" "${!unified_hosts_fakenews_gambling_porn_social@}"
+restart_gas_mask
+cleanup
